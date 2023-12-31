d9e42d7f2ec0ac527a803e46dbbdad3731db7066
ssssccccchhhhh <chnghyn@gmail.com>
2023-12-31 10:09:17 +0900

    함수형 컴포넌트로 수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    const name = 'hello react';
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            {name}
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const name = 'hello react';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          {name}
+      </header>
+    </div>
+  );
 }
 
 export default App;
